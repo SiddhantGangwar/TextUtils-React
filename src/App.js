f5ea5d231e2e2596e8ff79263a5e4976c6a0cb9b
SiddhantGangwar <engineersiddhantcse@gmail.com>
2023-05-05 17:29:12 +0530

    TextUtils almost Complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,69 @@
 // import logo from './logo.svg';
+import { useState } from "react";
 import "./App.css";
 // import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
+
+// import {
+// 	BrowserRouter as Router,
+// 	Routes,
+// 	Route,
+// } from 'react-router-dom';
+
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert =(message,type)=>{
+        setAlert({
+          msg : message,
+          type : type
+        })
+        setTimeout(() => {
+          setAlert(null);
+        }, 1500);
+  }
+
+  const toggleMode = () =>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor='#042743';
+      showAlert("Dark mode has been enabled","success");
+      document.title = 'TextUtils - Dark Mode';
+      // setInterval(() => {
+      //   document.title = 'TextUtils is amazing';
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title = 'Install TextUtils';
+      // }, 1500);
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      showAlert("Light mode has been enabled","success");
+      document.title = 'TextUtils - Light Mode';
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" aboutText="aboutTextUtils" />
+    {/* <Router> */}
+      <Navbar title="TextUtils" aboutText="aboutTextUtils" mode={mode} toggleMode={toggleMode}/>
       {/* <Navbar /> */}
+      <Alert alert={alert}/>
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze below" />
-        {/* <About/> */}
+      {/* <switch>
+        <Routes>
+         <Route exact path="/about" element={<About/>}/>
+         <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>}/>
+         </Routes>
+      </switch> */}
+         <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
+        {/* <About/>  */}
       </div>
+      {/* </Router> */}
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,16 +5,19 @@ export default function TextForm(props) {
     // console.log("Uppercase was clicked");
     let newText = text.toUpperCase();
     setText(newText);
+    props.showAlert("Converted to UpperCase","success");
   };
   const handleClearClick = () => {
     // console.log("Uppercase was clicked");
     let newText = "";
     setText(newText);
+    props.showAlert("Text is cleared","success");
   };
   const handleloClick = () => {
     // console.log("Uppercase was clicked");
     let newText = text.toLowerCase();
     setText(newText);
+    props.showAlert("Converted to LowerCase","success");
   };
   const handleOnChange = (event) => {
     // console.log("OnChange");
@@ -25,15 +28,17 @@ export default function TextForm(props) {
     var newText = document.getElementById("myBox");
     newText.select();
     navigator.clipboard.writeText(newText.value);
+    props.showAlert("Text Copied to Clipboard!","success");
   };
-  const handleExtraSpaces = () =>{
-    let newText= text.split(/[ ]+/);
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
     setText(newText.join(" "));
+    props.showAlert("Extra Spaces are removed","success");
   }
   const [text, setText] = useState("");
   return (
     <>
-      <div className="container">
+      <div className="container" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
           {/* <label for="myBox" class="form-label">Example textarea</label> */}
@@ -42,6 +47,7 @@ export default function TextForm(props) {
             id="myBox"
             value={text}
             onChange={handleOnChange}
+            style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }}
             rows="8"
           ></textarea>
         </div>
@@ -61,12 +67,12 @@ export default function TextForm(props) {
           Remove Extra Space
         </button>
       </div>
-      <div className="container my-3">
+      <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
         <h2>Your Text Summary</h2>
         <p>{text.split(" ").length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").length} Minutes Read</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0 ? text:'Enter something in the text box above to preview it'}</p>
       </div>
     </>
   );
